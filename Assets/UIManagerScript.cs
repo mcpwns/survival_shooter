5b48019fcd4efc100071156534113362d5fdbd8b
mcpwns <matthewcheung520@hotmail.com>
2016-10-31 16:03:08 -0400

    Added start screen and .gitignore file

---
+++ Assets/UIManagerScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManagerScript : MonoBehaviour {
+
+	public void StartGame() {
+		SceneManager.LoadScene ("Level 01");
+	}
+}
